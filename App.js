4cc948fe5feb11174f7710974467f1fc7aac32b9
Mahela Sulakkhana <120177044+s17312@users.noreply.github.com>
2023-05-01 20:00:25 +0530

    Adding my new project
    started to  create a film store app using React tailwind

---
+++ App.js
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={ <Home/>} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
